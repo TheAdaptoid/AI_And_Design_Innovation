1fb5a6bf5eae2273b3d0e9826f59ad67ea96827f
TheAdaptoid <mhwkmn22@gmail.com>
2025-03-27 23:16:21 -0400

    UI implementation

---
+++ main.py
@@ -0,0 +1,9 @@
+from nicegui import ui
+from frontend.index import index
+
+if __name__ in {"__main__", "__mp_main__"}:
+    ui.run(
+        title="Jaxon",
+        dark=False,
+        reload=True,
+    )
